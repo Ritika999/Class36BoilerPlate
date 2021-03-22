830859e2c77a2066c9871c0c88956f26c2c2731f
Ritika999 <70030520+Ritika999@users.noreply.github.com>
2021-03-22 12:33:34 +0530

    Dheer C36

--- carRacingStage0.5/js/Form.js
+++ carRacingStage0.5/js/Form.js
@@ -8,25 +8,25 @@ class Form {
     title.html("Car Racing Game");
     title.position(130, 0);
     
-    var textbox = createInput("Name");
+    var input = createInput("Name");
     var button = createButton('Play');
     
-    textbox.position(130, 160);
+    input.position(130, 160);
     button.position(250, 200);
 
-  button.mousePressed(function(){
-    textbox.hide();    //hiding the textbox and button once the button is clicked
-    button.hide();
+    button.mousePressed(function(){
+      input.hide();
+      button.hide();
 
-    var name = textbox.value(); //storing the value of textbox in variable name
-    
-    playerCount+=1;
-    player.update(name)
-    player.updateCount(playerCount);
-    var greeting = createElement('h3');
-    greeting.html("Hello " + name )
-    greeting.position(130, 160)
-  });
+      var name = input.value();
+      
+      playerCount+=1;
+      player.update(name)
+      player.updateCount(playerCount);
+      var greeting = createElement('h3');
+      greeting.html("Hello " + name )
+      greeting.position(130, 160)
+    });
 
   }
 }
