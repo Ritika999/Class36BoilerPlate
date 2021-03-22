65786b3a623c5fb2acaa721a5424e7bfb1a9d6ad
Ritika999 <70030520+Ritika999@users.noreply.github.com>
2021-03-22 12:18:54 +0530

    Dheer C36

--- carRacingStage0.5/js/Form.js
+++ carRacingStage0.5/js/Form.js
@@ -4,7 +4,29 @@ class Form {
   }
 
   display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(130, 0);
     
+    var textbox = createInput("Name");
+    var button = createButton('Play');
+    
+    textbox.position(130, 160);
+    button.position(250, 200);
+
+  button.mousePressed(function(){
+    textbox.hide();    //hiding the textbox and button once the button is clicked
+    button.hide();
+
+    var name = textbox.value(); //storing the value of textbox in variable name
+    
+    playerCount+=1;
+    player.update(name)
+    player.updateCount(playerCount);
+    var greeting = createElement('h3');
+    greeting.html("Hello " + name )
+    greeting.position(130, 160)
+  });
 
   }
 }
